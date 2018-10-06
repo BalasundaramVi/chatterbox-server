f0f9555bfdaf6155e74b975adcc577272d90c3c3
BalasundaramVi <vigneshbalasundaram@gmail.com>
2018-10-06 15:58:09 -0700

    remove submodule

--- hrsf105-chatterbox-client/client/scripts/app.js
+++ hrsf105-chatterbox-client/client/scripts/app.js
@@ -6,7 +6,7 @@ var App = {
 
   initialize: function() {
     App.username = window.location.search.substr(10);
-
+    debugger;
     FormView.initialize();
     RoomsView.initialize();
     MessagesView.initialize();
@@ -18,13 +18,16 @@ var App = {
 
     // Poll for new messages every 3 sec
     setInterval(App.fetch, 3000);
-      },
+  },
 
   fetch: function(callback = ()=>{}) {
     Parse.readAll((data) => {
 
       // Don't bother to update if we have no messages
-      if (!data.results || !data.results.length) { return; }
+      if (!data.results || !data.results.length) { 
+        callback();
+        return; 
+      }
 
       Rooms.update(data.results, RoomsView.render);
       Messages.update(data.results, MessagesView.render);
--- hrsf105-chatterbox-client/client/scripts/parse.js
+++ hrsf105-chatterbox-client/client/scripts/parse.js
@@ -14,18 +14,17 @@ var Parse = {
         console.error('chatterbox: Failed to create message', error);
       }
     });
-      },
+  },
 
   readAll: function(successCB, errorCB = null) {
     $.ajax({
       url: Parse.server,
       type: 'GET',
-      data: { order: '-createdAt' },
       contentType: 'application/json',
       success: successCB,
       error: errorCB || function(error) {
         console.error('chatterbox: Failed to fetch messages', error);
-        console.log()
+        console.log();
       }
     });
   }
